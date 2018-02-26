97edafb49c2a9d7f6b679969cf1eb1607d78a6cc
matthias-knopp@gmx.net <matthias-knopp@gmx.net>
2018-02-26 21:23:20 +0100

    added language switching support

--- aschexplorer/src/App.vue
+++ aschexplorer/src/App.vue
@@ -7,6 +7,10 @@
         <router-link to="/block">{{ $t("BLOCK") }}</router-link>
         <router-link to="/tx">Transactions</router-link>
         <router-link to="/address">Addresses</router-link>
+        <div>
+          <button @click="setNewLang('de')">de</button>
+          <button @click="setNewLang('en')">en</button>
+        </div>
       </header>
     </headroom>
     <router-view/>
@@ -15,7 +19,14 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    setNewLang (newLang) {
+      if (typeof newLang === 'string') {
+        this.$i18n.locale = newLang
+      }
+    }
+  }
 }
 </script>
 
--- aschexplorer/src/localization/index.js
+++ aschexplorer/src/localization/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import en from '@/localization/en-US.json'
+import en from '@/localization/en.json'
+import de from '@/localization/de.json'
 
 Vue.use(VueI18n)
 
 const i18n = new VueI18n({
   locale: 'en',
   messages: {
-    en
+    en,
+    de
   }
 })
 
